efbc3d28d5122316bb2125e9b2133d5a0519da8f
dilyara <di.salakhovaa@mail.ru>
2023-02-06 15:03:09 +0300

    Search and modal

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,4 @@
-import productImage from "../../assets/product_1.png";
+import formatMoney from "../../utils/formatMoney";
 import { HeartIcon, PlusIcon } from "../Icons/Icons";
 
 const Product = ({ product }) => {
@@ -8,14 +8,14 @@ const Product = ({ product }) => {
                 <HeartIcon />
             </div>
 
-            <img src={productImage} alt="" />
+            <img src={product.preview} alt={product.name} />
 
-            <h3>Наименование продукта</h3>
+            <h3>{product.name}</h3>
 
             <div className="product__footer">
                 <div className="price">
                     <span className="gray">Цена:</span>
-                    <span className="value">12 999 руб.</span>
+                    <span className="value">{formatMoney(product.price)}</span>
                 </div>
 
                 <button>
